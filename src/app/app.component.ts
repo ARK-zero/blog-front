c07157d21f76fc9cc3b745e6464dbf39eb763e3a
linpengfei <a569836198@vip.qq.com>
2018-04-08 18:57:05 +0800

    新增备案信息

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,17 @@ import {UserService} from './user';
 
 @Component({
   selector: 'app-root',
-  template: `    
-    <!--<p-scrollPanel [style]="{width: '100%', height: '100%'}">-->
+  template: `
       <router-outlet></router-outlet>
       <p-growl [life]="2000"></p-growl>
-    <!--</p-scrollPanel>-->
+    		 	<div class="beian">
+            <span>Copyright © 2018 - 2018  pengfeilin.cn 版权所有</span>
+            <span>浙ICP备18012224号-1</span>
+		 		<a target="_blank" 
+           href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=33010802009018">
+          <img src="assets/images/beian.png" style="float:left;"/>
+          <p style="float:left;height:20px;line-height:20px;margin: 0px 0px 0px 5px; color:#939393;">浙公网安备 33010802009018号</p></a>
+		 	</div>
   `
 })
 export class AppComponent {
